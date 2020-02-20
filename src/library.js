2ed26281638df7eb9fbbdb2d56814fc4da8b39b8
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-20 22:48:13 +0100

    [Feature] Add book object constructor and addtoLibrary function

--- src/library.js
+++ src/library.js
@@ -1,3 +1,24 @@
-function Book() {
-  
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = status;
+  this.info = function() {
+    return `Book Title: ${title}, Author: ${author}, Pages: ${pages}, Book Read?: ${status}.`;
+  };
 }
+
+let book = new Book('Gulliver Travels', 'Chistopher Nolan', 50, true);
+
+
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book(title, author, pages, status);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary('Gulliver Travels', 'Chistopher Nolan', 50, true);
+
+console.log(myLibrary);
+console.log(myLibrary[0].info());
