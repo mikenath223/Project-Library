8f9b60b1367af2aee07bf8ef7d2a81bf6635f084
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-21 18:05:08 +0100

    [Feature] Create add book button to toggle form display

--- src/library.js
+++ src/library.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
-const getLibrary = document.querySelector('.my-books');
+const selectQuery = (query) => document.querySelector(query);
+const createElement = (elem) => { return document.createElement(elem)};
+const appendChild = (parent, child) => { return parent.appendChild(child)};
+
+const getLibrary = selectQuery('.my-books');
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -14,45 +18,54 @@ function addBookToLibrary(title, author, pages, status) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('Gulliver Travels', 'Chistopher Nolan', 50, true);
-addBookToLibrary('Gulliver Travels II', 'Chistopher Nolan', 500, false);
-addBookToLibrary('Gul Plays', 'Chisan Lopez', 150, true);
 
-
-let removeButtons = (elem) => {
+let removeBooks = (elem) => {
   elem.addEventListener('click', function(e){
-  console.log(e.target.dataset.index)
+    const bookIndex = e.target.dataset.index;
+  delete myLibrary[bookIndex];
+  const book = selectQuery(`div[data-index="book${bookIndex}"]`);
+  document.querySelector('.my-books').removeChild(book);
 })}
+
 
 
 function render() {
 
   const len = myLibrary.length;
-  const parents = document.createElement('div')
-  const element = document.createElement('h1');
-  const button = document.createElement('button');
+  const parents = createElement('div');
+  const element = createElement('h1');
+  const button = createElement('button');
   button.innerHTML = 'Remove'
   button.setAttribute('type', 'submit')
-  parents.classList.add('book')
-  element.innerHTML = myLibrary[len - 1].info();
-  getLibrary.appendChild(parents);
-  parents.appendChild(element);
-  parents.appendChild(button);
-  button.dataset.index =  len - 1;
+  parents.classList.add('book');
+  appendChild(getLibrary, parents);
+  appendChild(parents, element);
+  appendChild(parents, button);
+  if (len >= 1) {
+    element.innerHTML = myLibrary[len - 1].info()
+    button.dataset.index =  len - 1;
+    parents.dataset.index =  `book${len - 1}`;
+  }
   button.classList.add('removeBook');
-  button.addEventListener('click', removeButtons(button));
+  button.addEventListener('click', removeBooks(button));
 }
 
-render();
 
+(function(){
+  const createBookButton = selectQuery('.createBookButton');
+ const addBookForm = selectQuery('.createBookForm');
+ createBookButton.addEventListener('click', function(e) {
+   addBookForm.classList.toggle('displayForm');
+ })
+})();
 
 function displayLibrary() {
   const submitBut = document.getElementById('submit');
   document.getElementById('form').onsubmit = (e) => e.preventDefault();
   submitBut.addEventListener('click', () => {
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const pages = document.querySelector('#pages').value;
+    const title = selectQuery('#title').value;
+    const author = selectQuery('#author').value;
+    const pages = selectQuery('#pages').value;
     const readElem = document.querySelectorAll('input[type="radio"]');
     let readStatus;
     readElem.forEach((elem) => {
