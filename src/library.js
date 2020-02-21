4aefc3aa85fca12af6d600c273f3b29579468d24
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-21 01:35:54 +0100

    [Feature] Create call render function on js

--- src/library.js
+++ src/library.js
@@ -19,18 +19,46 @@ addBookToLibrary('Gulliver Travels II', 'Chistopher Nolan', 500, false);
 addBookToLibrary('Gul Plays', 'Chisan Lopez', 150, true);
 
 
-
 function render() {
-
-    myLibrary.forEach(item =>  {
-      
-             
-        let element = document.createElement('h1');
-        element.innerHTML = item.info();
-        getLibrary.appendChild(element)
-        
-    }
-    )
+  const len = myLibrary.length;
+  const element = document.createElement('h1');
+  element.innerHTML = myLibrary[len - 1].info();
+  getLibrary.appendChild(element);
 }
 
 render();
+
+function displayLibrary() {
+  const submitBut = document.getElementById('submit');
+  document.getElementById('form').onsubmit = (e) => e.preventDefault();
+  submitBut.addEventListener('click', () => {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const readElem = document.querySelectorAll('input[type="radio"]');
+    let readStatus;
+    readElem.forEach((elem, i) => {
+      if (readElem[i].checked) {
+        readStatus = readElem[i].value;
+      }
+    });
+
+    const checkLen = (inputVal) => {
+      if (inputVal.length >= 1) {
+        return true;
+      }
+      return false;
+    };
+
+    function callRenderOnValid() {
+      if (checkLen(title) && checkLen(author) && checkLen(pages)) {
+        addBookToLibrary(title, author, pages, readStatus);
+        render();
+      }
+    }
+
+    callRenderOnValid();
+  });
+}
+
+displayLibrary();
